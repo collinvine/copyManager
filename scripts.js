bf6ea8166ac1633d40d9f34bcd2cd02a6a15278f
Collin Vine <collinvine@gmail.com>
2019-05-20 14:44:00 -0700

    Fix listeners and and get copy function working properly

--- scripts.js
+++ scripts.js
@@ -1,68 +1,72 @@
 // grab elements
-const inputElement = document.querySelector('input');
 const listElement = document.querySelector('.list');
-
-// create new elements and add class list
-const addMove = document.createElement('i');
-const addContent = document.createElement('div');
-const addCopy = document.createElement('i');
-const addDelete = document.createElement('i');
-
-addMove.classList.add("fas", "fa-grip-vertical", "drag");
-addContent.classList.add("content");
-addCopy.classList.add("fas", "fa-copy", "copy", "icon", "push");
-addDelete.classList.add("fas", "fa-times", "delete", "icon", "edit");
+const inputElement = document.querySelector('input');
 
 // focus curson in input on load
-window.onload = inputElement.focus();
-
-// add return key listen and call addItem function
-inputElement.addEventListener('keydown', function() {
-  if (event.code == 'Enter') {
-    addItem();
-    inputElement.value = "";
+window.onload = () => {
     inputElement.focus();
-  }
-});
+    inputElement.addEventListener('keydown', function() {
+        if (event.code === 'Enter' || event.code === "NumpadEnter") {
+            addItem();
+            inputElement.value = "";
+            inputElement.focus();
+        };
+    });
+};
 
 // add event listeners
-const copy = function(e) {
-  const itemText = e.target.previousElementSibling.innerText;
-  itemText.select();
-  document.execCommand("copy");
-  console.log(itemText);
-}
+const copy = event => {
+    const range = document.createRange();
+    const selection = window.getSelection();
 
-const remove = function(e) {
-  e.target.parentElement.remove();
-}
+    selection.removeAllRanges();
+    range.selectNodeContents(event.target.previousElementSibling);
+    selection.addRange(range);
+    document.execCommand('copy');
+};
 
-const copyListener = function(addCopy) {
-    console.log(addCopy);
-    addCopy.addEventListener("click", copy)
-}
+const remove = event => {
+    event.target.parentElement.remove();
+};
 
-const deleteListener = function(addDelete) {
-    console.log(addDelete);
-    addDelete.addEventListener("click", remove)
-}
+const copyListener = addCopy => {
+    addCopy.addEventListener("click", copy);
+};
 
+const deleteListener = addDelete => {
+    addDelete.addEventListener("click", remove);
+};
 
 // add pasted item to list
-const addItem = function() {
-  const enteredText = inputElement.value;
-  addContent.textContent = enteredText;
+const addItem = () => {
+    // create new elements and add class list
+    const addMove = document.createElement('i');
+    const addContent = document.createElement('div');
+    const addCopy = document.createElement('i');
+    const addDelete = document.createElement('i');
+    const contentContainer = document.createElement('div');
 
-  const contentContainer = document.createElement('div');
-  contentContainer.classList.add("item");
+    addMove.classList.add("fas", "fa-grip-vertical", "drag");
+    addContent.classList.add("content");
+    addCopy.classList.add("fas", "fa-copy", "copy", "icon", "push");
+    addDelete.classList.add("fas", "fa-times", "delete", "icon", "edit");
+    contentContainer.classList.add("item");
 
-  // add elements to div
-  contentContainer.innerHTML = addMove.outerHTML + addContent.outerHTML + addCopy.outerHTML + addDelete.outerHTML;
+    // get value of entered text
+    const enteredText = inputElement.value;
+    addContent.textContent = enteredText;
 
-  // add item to top of the list
-  const firstListItem = listElement.firstChild;
-  listElement.insertBefore(contentContainer, firstListItem);
+    // add elements to content container div
+    contentContainer.appendChild(addMove);
+    contentContainer.appendChild(addContent);
+    contentContainer.appendChild(addCopy);
+    contentContainer.appendChild(addDelete);
 
-  copyListener(addCopy);
-  deleteListener(addDelete);
+    // add div to top of the list
+    const firstListItem = listElement.firstChild;
+    listElement.insertBefore(contentContainer, firstListItem);
+
+    // add listners
+    copyListener(addCopy);
+    deleteListener(addDelete);
 };
